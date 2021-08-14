1e331ffc11ab62a35233a3d3460fc8a02d7031ce
JulioAvalos <julioavalos2@msn.com>
2021-08-13 22:55:44 -0600

    primer commit del proyecto

---
+++ src/main/java/com/blockchain/anonimos/AnonimosApplication.java
@@ -0,0 +1,13 @@
+package com.blockchain.anonimos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonimosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonimosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blockchain/anonimos/AnonimosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockchain.anonimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonimosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
